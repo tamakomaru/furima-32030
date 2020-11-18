6127f72302f8c3823dc6797f1b69b9c0c14b7851
yuki <tamakomaru@users.noreply.github.com>
2020-11-18 16:12:05 +0900

    新規登録のバリデーション設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,11 +2,22 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+        :recoverable, :rememberable, :validatable
+
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください' 
+
   validates :nickname, presence: true
-  validates :last_name, presence: true
-  validates :first_name, presence: true
-  validates :last_kana, presence: true
-  validates :first_kana, presence: true
+
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: '全角文字を使用してください' } do
+    validates :first_name
+    validates :last_name
+  end
+
+  with_options presence: true, format: { with: /\A[ァ-ン]+\z/, message: 'カタカナ(全角)を使用してください' } do
+    validates :last_kana
+    validates :first_kana
+  end
   validates :birthday, presence: true
+  
 end
