21caf6d044f077f0f009098f43a9deec3c560480
yuki <tamakomaru@users.noreply.github.com>
2020-11-19 14:28:13 +0900

    バリデーション修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,18 +1,23 @@
 class Item < ApplicationRecord
-belongs_to :user
-has_one :purchase_info
-belongs_to_active_hash :category
-belongs_to_active_hash :condition
-belongs_to_active_hash :shipping_charge
-belongs_to_active_hash :prefectures
-belongs_to_active_hash :purchase_to_day
 
-validates :item_name, :text, presence: true
-validates :explanation, :text, presence: true
+  belongs_to :user
+  has_one :purchase_info
 
-validates :category_id, numericality: { other_than: 1 }
-validates :condition_id, numericality: { other_than: 1 }
-validates :shipping_charge_id, numericality: { other_than: 1 }
-validates :prefectures_id, numericality: { other_than: 1 }
-validates :purchase_to_day_id, numericality: { other_than: 1 }
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :shipping_charge
+  belongs_to :prefectures
+  belongs_to :purchase_to_day
+
+  with_options presence: true do
+  validates :item_name
+  validates :explanation
+  validates :category_id, numericality: { other_than: 1 }
+  validates :condition_id, numericality: { other_than: 1 }
+  validates :shipping_charge_id, numericality: { other_than: 1 }
+  validates :prefectures_id, numericality: { other_than: 1 }
+  validates :purchase_to_day_id, numericality: { other_than: 1 }
+  end
+
 end
