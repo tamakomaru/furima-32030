fbd20f3076fb019e4bf104c0b164f40c40067201
yuki <tamakomaru@users.noreply.github.com>
2020-11-19 14:17:55 +0900

    ストロングパラメーター設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,4 +2,24 @@ class ItemsController < ApplicationController
   def index
     
   end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:item_name,:explanation,:category_id,:condition_id,:shipping_charge_id,:prefectures_id,:purchase_to_day_id)
+  end
+
 end
