820456a5c7704a00f623c6ba70035067a37ad5b0
yuki <tamakomaru@users.noreply.github.com>
2020-11-23 15:53:46 +0900

    削除機能の追加

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :edit, :update]
-  before_action :set_item, only: [:show,:edit,:update]
-  before_action :move_to_index, only: [:edit]
+  before_action :set_item, only: [:show,:edit,:update,:destroy]
+  before_action :move_to_index, only: [:edit,:destroy]
   def index
     @items = Item.includes(:user).order('id DESC')
   end
@@ -30,6 +30,12 @@ class ItemsController < ApplicationController
       redirect_to item_path
     else
       render :edit
+    end
+  end
+
+  def destroy
+    if @item.destroy
+      redirect_to root_path
     end
   end
 
