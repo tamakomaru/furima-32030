91c2377a4bec78b937e2764db9eddce5e9fd79ab
yuki <tamakomaru@users.noreply.github.com>
2020-11-23 14:28:40 +0900

    ログイン状態の出品者だけが商品情報編集ページに遷移できる

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :edit, :update]
+  before_action :move_to_index, only: [:edit]
   def index
     @items = Item.includes(:user).order('id DESC')
   end
@@ -34,9 +35,18 @@ class ItemsController < ApplicationController
     end
   end
 
+
   private
 
   def item_params
     params.require(:item).permit(:item_name, :explanation, :category_id, :condition_id, :shipping_charge_id, :prefecture_id, :purchase_to_day_id, :value, :image).merge(user_id: current_user.id)
   end
+
+  def move_to_index
+    @item = Item.find(params[:id])
+    unless user_signed_in? && current_user.id == @item.user_id
+      redirect_to action: :index
+    end  
+  end
+    
 end
