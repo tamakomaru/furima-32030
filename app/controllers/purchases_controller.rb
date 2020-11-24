29a792dd28d10a60051075df3843898bf92d9ce1
yuki <tamakomaru@users.noreply.github.com>
2020-11-24 11:18:02 +0900

    コントローラー設定

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -5,4 +5,15 @@ class PurchasesController < ApplicationController
     @item_purchase = ItemPurchase.new
   end
 
+  def create
+    @item_purchase = ItemPurchase.new(purchase_params)
+
+  end
+
+  private
+
+  def purchase_params
+    params.require(:item_purchase).permit(:user_id, :item_id, :postal_code, :prefecture_id, :city, :address, :building, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id])
+  end 
+
 end
