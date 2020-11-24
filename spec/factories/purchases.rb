15b090261abae0d01da5f768a18c39b87d52356a
yuki <tamakomaru@users.noreply.github.com>
2020-11-24 12:55:36 +0900

    テストコード

--- spec/factories/purchases.rb
+++ spec/factories/purchases.rb
@@ -1,5 +1,12 @@
 FactoryBot.define do
   factory :purchase do
-    
+    postal_code {'111-2222'}
+    prefectures_id              { Faker::Number.between(from: 2, to: 3) }
+    city {'京都市'}
+    building {'ハイツ'}
+    address {'1-2-3'}
+    phone_number {'08000000000'}
+    user_id{1}
+    item_id{1}
   end
 end
