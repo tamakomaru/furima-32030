9c11bb57426c04b548e88a61736d43b31e0b6285
yuki <tamakomaru@users.noreply.github.com>
2020-11-25 15:42:26 +0900

    コントローラーアクション修正

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -2,8 +2,7 @@ class PurchasesController < ApplicationController
   before_action :set_item, only: [:index, :create]
   before_action :authenticate_user!, only: [:index, :create]
   before_action :purchase_item_confirm, only: [:index]
-  before_action :user_purchase_confirm, only: [:index]
-  
+
   def index
     @item_purchase = ItemPurchase.new
   end
@@ -14,7 +13,7 @@ class PurchasesController < ApplicationController
     if @item_purchase.valid?
       pay_item
       @item_purchase.save
-      return redirect_to root_path
+      redirect_to root_path
     else
       render action: :index and return
     end
@@ -24,30 +23,22 @@ class PurchasesController < ApplicationController
 
   def purchase_params
     params.require(:item_purchase).permit(:user_id, :item_id, :postal_code, :prefecture_id, :city, :address, :building, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
-  end 
+  end
 
   def set_item
     @item = Item.find(params[:item_id])
   end
 
   def pay_item
-    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+    Payjp.api_key = ENV['PAYJP_SECRET_KEY']
     Payjp::Charge.create(
-        amount: @item.value,  
-        card: purchase_params[:token],    
-        currency: 'jpy'                 
-      )
+      amount: @item.value,
+      card: purchase_params[:token],
+      currency: 'jpy'
+    )
   end
-  
-  def purchase_item_confirm
-    if @item.purchase.present?
-      redirect_to root_path
-    end  
-  end
-  
-  def user_purchase_confirm
-    if @item.user_id == current_user.id
-      redirect_to root_path
-    end
 
+  def purchase_item_confirm
+    redirect_to root_path if @item.purchase.present? || @item.user_id == current_user.id
+  end
 end
