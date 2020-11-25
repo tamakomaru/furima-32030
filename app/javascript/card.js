484db2de15a01485cf9d20e9b7f87204aee7cc3b
yuki <tamakomaru@users.noreply.github.com>
2020-11-25 15:42:43 +0900

    コード消し忘れ修正

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -13,8 +13,6 @@ const pay = () => {
       exp_month: formData.get("item_purchase[exp_month]"),
       exp_year: `20${formData.get("item_purchase[exp_year]")}`,
     };
-    
-    console.log(card);
 
     Payjp.createToken(card, (status, response) => {
       if (status == 200) {
